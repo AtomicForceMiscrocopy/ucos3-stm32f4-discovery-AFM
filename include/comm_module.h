92225cb5d946261c7fc46fba8764443439a95cfe
Livincent-Zhang <LivincentZhang@gmail.com>
2015-04-23 10:49:19 +0800

    update daily

--- include/comm_module.h
+++ include/comm_module.h
@@ -6,12 +6,12 @@
 
 #define MOD_COMM_HEAD					0x0001
 
-#define MOD_COMM_TASK_SEND				0x00
+#define MOD_COMM_TASK_SEND					0x00
 
-#define MOD_COMM_CMD_SEND_INT			0x00
-#define MOD_COMM_CMD_BOARD_SEND_INT		0x02
+#define MOD_COMM_CMD_SEND_INT					0x00
+#define MOD_COMM_CMD_BOARD_SEND_INT					0x01
 
-#define MOD_COMM_TASK_REPORT			0X01
+#define MOD_COMM_TASK_REPORT					0X01
 
 OS_Q	SendDataQ;
 
